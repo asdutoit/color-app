6fce90acd0f0fba6604ab018304b6cb41166e379
Stephan Du Toit <stephandutoit@Stephans-MacBook-Pro.local>
2019-10-28 09:44:31 +0200

    first commit

---
+++ src/Palette.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Palette extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Palette Component</h1>
+      </div>
+    );
+  }
+}
