6a687bc148568e2afb239998740762b9c7fb4380
Stephan Du Toit <asdutoit@gmail.com>
2019-11-01 10:01:14 +0200

    added drawer, color picker and buttons

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import seedColors from "./seedcolors";
 import { generatePalette } from "./ColorHelpers";
 import PaletteList from "./PaletteList";
 import SingleColorPalette from "./SingleColorPalette";
+import NewPaletteForm from "./NewPaletteForm";
 
 class App extends React.Component {
   findPalette(id) {
@@ -15,6 +16,7 @@ class App extends React.Component {
   render() {
     return (
       <Switch>
+        <Route exact path="/palette/new" render={() => <NewPaletteForm />} />
         <Route
           exact
           path="/"
